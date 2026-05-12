9020e430c93721f4ed4c4e93da7667530d379b57
R0666 <r.jerez@duocuc.cl>
2026-05-11 23:01:05 -0400

    Arreglo AppRoutes, lo había matado. Aún quedan arreglos por hacer.... ej: log in está siendo obligatorio

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,21 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Landing from './pages/Landing'
-import Login from './pages/Login'
-import Perfil from './pages/Perfil'
-import Catalogo from './pages/Catalogo'
-import Producto from './pages/Producto'
-import Pedido from './pages/Pedido'
-import Envio from './pages/Envio'
+import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
+import AppRoutes from './app/AppRoutes'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="min-h-screen flex flex-col bg-white text-gray-900">
-        <Navbar />
-        <main className="flex-1">
-          <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/perfil" element={<Perfil />} />
-            <Route path="/catalogo" element={<Catalogo />} />
-            <Route path="/producto/:id" element={<Producto />} />
-            <Route path="/pedido/:id" element={<Pedido />} />
-            <Route path="/envio/:id" element={<Envio />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <AuthProvider>
+        <div className="min-h-screen flex flex-col bg-white text-gray-900">
+          <Navbar />
+          <main className="flex-1">
+            <AppRoutes />
+          </main>
+          <Footer />
+        </div>
+      </AuthProvider>
     </BrowserRouter>
   )
 }
